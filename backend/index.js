e82e65ae7bc49af1b521f83874dc13adfd041a1f
SAHIL AHMAD <sahilahmad3504@gmail.com>
2025-02-06 01:09:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,18 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: '*',  
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  
-  allowedHeaders: ['Content-Type', 'Authorization', 'Host']  
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
+  allowedHeaders: [
+    'Content-Type', 
+    'Authorization', 
+    'Host',
+    'username',    
+    'password'     
+  ],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 
 
